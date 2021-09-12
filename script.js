ce3076b269a892cc8cbb0ed9b9590319f70b6ac2
sahassa-6210513063 <71627277+sahassa-6210513063@users.noreply.github.com>
2021-09-13 05:33:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let openRegisterRight = document.querySelector('.h1');
+let registerwrapper = document.querySelector(".register-wrapper");
+
+openRegisterRight.addEventListener('click',function() {
+    registerwrapper.classList.toggle('open');
+});
